3453c01ea44a979fbf61586f293289809118b114
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-14 22:52:56 -0700

    nm

--- lib/src/controller/components/profile_button.dart
+++ lib/src/controller/components/profile_button.dart
@@ -3,26 +3,35 @@ import 'package:flutter/material.dart';
 import '../constant/images.dart';
 import 'black_text.dart';
 class ProfileButton extends StatelessWidget {
-  const ProfileButton({super.key});
+
+   VoidCallback onTap;
+   String text;
+   IconData icon;
+
+   ProfileButton({super.key,
+     required this.onTap,
+     required this.text,
+     required this.icon,
+   });
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.sizeOf(context).height;
     final screenWidth = MediaQuery.sizeOf(context).width;
     return InkWell(
-      onTap: (){},
+      onTap: onTap,
       child: Container(
         padding: EdgeInsets.symmetric(vertical: screenHeight*.015),
         child: Row(children: [
           ImageIcon(AssetImage(AppImages.lock),color: Colors.green,),
           SizedBox(width: screenWidth*.02),
           BlackNormalText(
-            text: "About Me",
+            text: text,
             fontSize: 12,
             fontWeight: FontWeight.w600 ,
           ),
           Spacer(),
-          Icon(Icons.arrow_forward_ios_outlined,color: Colors.grey),
+          Icon(icon,color: Colors.grey),
 
         ],),
       ),

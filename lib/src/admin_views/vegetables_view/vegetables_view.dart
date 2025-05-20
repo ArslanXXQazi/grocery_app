8e52b3c0802dc090996bbf6232150b34b6b76ac6
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-20 04:41:30 -0700

    Update vegetables_view.dart

--- lib/src/admin_views/vegetables_view/vegetables_view.dart
+++ lib/src/admin_views/vegetables_view/vegetables_view.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../controller/components/black_text.dart';
+import '../../controller/constant/App_colors.dart' show AppColors;
+
 class VegetablesView extends StatelessWidget {
   const VegetablesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: AppColors.greyColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.greyColor,
+        centerTitle: true,
+        title: BlackNormalText(
+          text: "Vegetables",
+          fontSize: 22,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
     );
   }
 }

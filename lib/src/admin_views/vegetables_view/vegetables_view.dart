494c0e5e093f43c490253939903e9e8c167af96d
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-20 04:40:21 -0700

    Update vegetables_view.dart

---
+++ lib/src/admin_views/vegetables_view/vegetables_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VegetablesView extends StatelessWidget {
+  const VegetablesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

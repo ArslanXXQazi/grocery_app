022d2b0f85810f78a07467aba0115e3ed929f6ad
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-20 05:07:47 -0700

    Update vegetables_view.dart

--- lib/src/admin_views/vegetables_view/vegetables_view.dart
+++ lib/src/admin_views/vegetables_view/vegetables_view.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app/src/controller/components/apploader.dart';
 
 import '../../controller/components/black_text.dart';
 import '../../controller/constant/App_colors.dart' show AppColors;
@@ -23,7 +24,32 @@ class VegetablesView extends StatelessWidget {
       body: StreamBuilder(
           stream: FirebaseFirestore.instance.collection('Vegetable').snapshots(),
           builder: (context, snapshot){
-           
+            if(snapshot.connectionState == ConnectionState.waiting)
+              {
+                return AppLoader();
+              }
+          else if (snapshot.hasError)
+              {
+                return Center(child: BlackNormalText(text: "Error ${snapshot.hasError}",));
+              }
+            else if (!snapshot.hasData || snapshot.data!.docs.isEmpty)
+            {
+              return Center(child: BlackNormalText(text: "No Fruits Found",fontWeight: FontWeight.w700,fontSize: 25,));
+            }
+            else
+              {
+                return GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      crossAxisSpacing: 20,
+                      childAspectRatio: .7
+                    ),
+                    itemBuilder: (context, index)
+                    {
+                      var vegetables= snapshot.
+                    }
+                );
+              }
           }
       ),
     );

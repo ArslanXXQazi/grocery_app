d7b36a8b5db7f35fa5b3367416b6de7d3b72a30f
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-24 04:53:12 -0700

    Update product_detail_view.dart

--- lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_detail_view.dart
+++ lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_detail_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_app/src/controller/constant/App_colors.dart';
+import 'package:grocery_app/src/controller/constant/images.dart';
 
 class ProductDetailView extends StatelessWidget {
   const ProductDetailView({super.key});
@@ -9,6 +10,14 @@ class ProductDetailView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.greyColor,
       body: Column(children: [
+        Container(
+          height: 400,
+          width: double.infinity,
+          color: Colors.white,
+          child: Image(image: AssetImage(AppImages.peach)),
+        ),
+        const SizedBox(height: 20),
+        
 
       ],),
     );

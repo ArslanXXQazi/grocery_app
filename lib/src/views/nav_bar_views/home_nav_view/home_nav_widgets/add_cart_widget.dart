7269e6c3e18b6462b85dabeffa5dbb0a5d5bb572
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-24 04:48:09 -0700

    sd

--- lib/src/views/nav_bar_views/home_nav_view/home_nav_widgets/add_cart_widget.dart
+++ lib/src/views/nav_bar_views/home_nav_view/home_nav_widgets/add_cart_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:grocery_app/src/views/nav_bar_views/home_nav_view/product_detail_view/product_detail_view.dart';
 import '../../../../controller/components/black_text.dart';
 import '../../../../controller/constant/App_colors.dart';
 import '../../../../controller/constant/images.dart';
@@ -22,52 +25,55 @@ class AddCartWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Stack(
       children: [
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          width: 181,
-          margin: EdgeInsets.only(bottom: 20),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10)
+        GestureDetector(
+          onTap: (){Get.to(ProductDetailView());},
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            width: 181,
+            margin: EdgeInsets.only(bottom: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Column(
+              children: [
+                Image(image: AssetImage(image)),
+                BlackNormalText(
+                  text: "\$$price",
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12,
+                  textColor: Colors.green,
+                ),
+                BlackNormalText(
+                  text: name,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15,
+                ),
+                BlackNormalText(
+                  text: quantity,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12,
+                ),
+                const SizedBox(height: 10),
+                Divider(),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ImageIcon(AssetImage(AppImages.shopingBag),color: AppColors.greenColor,),
+                    const SizedBox(width: 5),
+                    BlackNormalText(
+                      onTap: addChartOnTap,
+                      text: "Add to cart",
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    )
+                  ],)
+
+              ],),
+
           ),
-          child: Column(
-            children: [
-              Image(image: AssetImage(image)),
-              BlackNormalText(
-                text: "\$$price",
-                fontWeight: FontWeight.w500,
-                fontSize: 12,
-                textColor: Colors.green,
-              ),
-              BlackNormalText(
-                text: name,
-                fontWeight: FontWeight.w500,
-                fontSize: 15,
-              ),
-              BlackNormalText(
-                text: quantity,
-                fontWeight: FontWeight.w500,
-                fontSize: 12,
-              ),
-              const SizedBox(height: 10),
-              Divider(),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  ImageIcon(AssetImage(AppImages.shopingBag),color: AppColors.greenColor,),
-                  const SizedBox(width: 5),
-                  BlackNormalText(
-                    onTap: addChartOnTap,
-                    text: "Add to cart",
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                  )
-                ],)
-
-            ],),
-
         ),
         Positioned(
           left: 130,
--- lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_detail_view.dart
+++ lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_detail_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/src/controller/constant/App_colors.dart';
 
 class ProductDetailView extends StatelessWidget {
   const ProductDetailView({super.key});
@@ -6,7 +7,10 @@ class ProductDetailView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.greyColor,
+      body: Column(children: [
 
+      ],),
     );
   }
 }

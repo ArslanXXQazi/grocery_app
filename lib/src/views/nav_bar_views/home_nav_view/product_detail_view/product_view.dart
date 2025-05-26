2f42dc59e8f518586915b5d28ad0766dde633937
Arslan Qazi <arslanqazi1999@gmail.com>
2025-05-25 23:12:44 -0700

    Update product_view.dart

--- lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_view.dart
+++ lib/src/views/nav_bar_views/home_nav_view/product_detail_view/product_view.dart
@@ -7,7 +7,23 @@ class ProductView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: BlackNormalText(
+            text: "Categories",
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: ImageIcon(AssetImage(AppImages.gear),color: Colors.black,))
+          ],
+        ),
+      ),
     );
   }
 }

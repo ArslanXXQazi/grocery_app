6ec530edf04701e75c6a07b813b6ef9ff74d80b9
Arslan Qazi <arslanqazi1999@gmail.com>
2025-06-01 07:50:43 -0700

    x,

---
+++ lib/src/views/nav_bar_views/profile_nav_view/my_order_view/my_order_view.dart
@@ -0,0 +1,25 @@
+
+
+import  'package:grocery_app/src/controller/constant/linker.dart';
+
+class MyOrderView extends StatelessWidget {
+  const MyOrderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(80),
+        child: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: BlackNormalText(
+            text: "My Order",
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}

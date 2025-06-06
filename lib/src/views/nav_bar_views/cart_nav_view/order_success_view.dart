9f1de7119ddc40c4e10224ef257092bb89d97c58
Arslan Qazi <arslanqazi1999@gmail.com>
2025-06-05 22:55:46 -0700

    Update order_success_view.dart

--- lib/src/views/nav_bar_views/cart_nav_view/order_success_view.dart
+++ lib/src/views/nav_bar_views/cart_nav_view/order_success_view.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:grocery_app/src/controller/constant/linker.dart';
+
 
 class OrderSuccessView extends StatelessWidget {
   const OrderSuccessView({super.key});
@@ -9,7 +10,22 @@ class OrderSuccessView extends StatelessWidget {
       body: Center(child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        
+        Image(image: AssetImage(AppImages.bag)),
+          const SizedBox(height: 20),
+          BlackNormalText(
+            text: "Your order was\nsuccessful !",
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+          const SizedBox(height: 20),
+          BlackNormalText(
+            text: "You will get a response within\na few minutes.",
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            textColor: Colors.grey,
+          ),
+
+
       ],),),
     );
   }
